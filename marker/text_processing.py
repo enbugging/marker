f8dbe419eae01c84a753f216ecf3f1b39d17a221
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2022-06-02 20:20:20 +0200

    More boldness calculation fixing

--- marker/text_processing.py
+++ marker/text_processing.py
@@ -1,27 +1,29 @@
+from operator import mod
 from nltk.corpus import stopwords
 from marker.position_rank import PositionRank
 import string
 import re
+import math
 
 def model_boldness(len, boldness = 1.0):
     if len <= 2: return [boldness] * len
     len -= 1
     a = len//2
-    b = (len + 1)//2
-    result = [(max(i, len - i) - a)/b * boldness for i in range(len+1)]
+    result = [max(i, len - i)/len * boldness for i in range(len+1)]
     return result
 
 def process(raw_text, \
-            maximum_number_of_words = 150, \
+            maximum_number_of_words = 40, \
             token_window_size = 7, \
             alpha = 0.15, 
-            boldness_baseline = 0.05):
+            boldness_baseline = 0.6):
     importance_assigner = \
         PositionRank(maximum_number_of_words, \
             token_window_size, \
             alpha)
     # Preprocessing text
     preprocess_text = list(enumerate(raw_text.split(' ')))
+    words_lenghts = [len(w) for (_, w) in preprocess_text]
     # Removing punctuation
     for i in range(len(preprocess_text)):
         w = preprocess_text[i][1]
@@ -42,19 +44,17 @@ def process(raw_text, \
     # Normalization
     for i in range(len(boldness_total_scores)):
         boldness_total_scores[i] /= min(maximum_number_of_words, min(i + 1, len(boldness_total_scores) - i))
+        boldness_total_scores[i] = math.log(boldness_total_scores[i] + 1)
     normalization = max(boldness_total_scores)
     for i in range(len(boldness_total_scores)):
-        boldness_total_scores[i] = \
-            boldness_total_scores[i] / normalization *  (1 - boldness_baseline) + \
-            boldness_baseline
+        boldness_total_scores[i] = boldness_total_scores[i] / normalization * (1 - boldness_baseline) + boldness_baseline
+    print(boldness_total_scores)
     # Calculate boldness for each character
     final_boldness = [0] * len(raw_text)
     cnt = 0
-    for (i, w) in preprocess_text:
-        final_boldness[cnt:cnt + len(w)] = model_boldness(len(w), boldness_total_scores[i])
-        cnt += len(w) + 1
-    for i in range(len(raw_text)):
-        if not (0 <= final_boldness[i] and final_boldness[i] <= 1):
-            print("Final score error:", i, final_boldness[i])
-            assert(False)
+    for i in range(len(words_lenghts)):
+        length = words_lenghts[i]
+        final_boldness[cnt:cnt + length] = model_boldness(length, boldness_total_scores[i])
+        print("It happens", model_boldness(length, boldness_total_scores[i]))
+        cnt += length + 1
     return final_boldness
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ def colored(r, g, b, text):
 
 text = 'It was supposed to be a dream vacation. They had planned it over a year in advance so that it would be perfect in every way. It had been what they had been looking forward to through all the turmoil and negativity around them. It had been the light at the end of both their tunnels. Now that the dream vacation was only a week away, the virus had stopped all air travel.'
 boldness = marker.text_processing.process(text)
+#print(boldness)
+#print(max(boldness))
 for i in range(len(text)):
     boldness[i] = int(boldness[i] * 255)
     print(colored(boldness[i], boldness[i], boldness[i], text[i]), end='')
